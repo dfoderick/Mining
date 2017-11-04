7933fdc7fd4b7de52d43482cfcb325388193783d
Dave <625411+dfoderick@users.noreply.github.com>
2017-11-03 17:47:07 -0700

    Fixed sed command

--- Antminer/antminerS9api_privileged.sh
+++ Antminer/antminerS9api_privileged.sh
@@ -3,7 +3,7 @@ ssh root@ipaddress.of.your.miner << EOF
 cd /config
 cp bmminer.conf bmminer_last.conf
 chmod u=rw bmminer.conf
-sed -i 's_^\("api-allow" : \).*_\1"W:your.ip.address,W:192.168.1.0/24,A:0/0"_' bmminer.conf
+sed -i 's_^\("api-allow" : \).*_\1"W:your.remote.ip.address,W:192.168.0.1/16,A:0/0",_' bmminer.conf
 chmod u=r bmminer.conf
 reboot
 EOF
